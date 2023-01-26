e2d0a24facb6d93d1ae9d44a962c71fb44a32ae8
Owaisraza <owaisharoon00@gmail.com>
2023-01-26 14:10:16 +0500

    Initial commit

---
+++ src/com/flappybird/controller/Controller.java
@@ -0,0 +1,19 @@
+package com.flappybird.controller;
+
+import com.flappybird.model.Bird;
+import java.awt.event.KeyEvent;
+
+public class Controller implements IStrategy {
+
+	@Override
+	public void controller(Bird bird, KeyEvent kevent) {
+	}
+
+	@Override
+	public void controllerReleased(Bird bird, KeyEvent kevent) {
+		if (kevent.getKeyCode() == KeyEvent.VK_SPACE || kevent.getKeyCode() == KeyEvent.VK_UP) {
+			bird.jump();
+		}
+	}
+
+}
